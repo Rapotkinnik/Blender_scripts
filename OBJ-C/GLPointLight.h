c0041fbb48141a7739d0a967dec3b39da5f967a7
Rapotkin Nikolay <Rapotkinnik@gmail.com>
2018-02-05 23:58:09 +0400

    Добавлен класс точечного источника света

---
+++ OBJ-C/GLPointLight.h
@@ -0,0 +1,37 @@
+//
+//  GLPointLight.h
+//  TestProject
+//
+//  Created by Рапоткин Николай on 05.02.18.
+//  Copyright (c) 2018 Rapotkin. All rights reserved.
+//
+
+#import "GLProgram.h"
+
+@interface BFGLPointLight : NSObject <BFGLDrawable>
+{
+	int m_lightId;
+    BOOL m_isLightOn;
+	float m_lightEnergy;
+    GLKVector3 m_position;
+    GLKVector3 m_ambientColor;
+    GLKVector3 m_diffuseColor;
+    GLKVector3 m_specularColor;
+    GLKVector3 m_lightAttenuation;
+}
+
+-(instancetype)pointLightWithPosition:(GLKVector3)position
+                              Ambient:(GLKVector3)ambient
+                              Diffuse:(GLKVector3)diffuse
+                             Specular:(GLKVector3)specular
+                          Attenuation:(GLKVector3)attenuation;
+
+@property (nonatomic, assign) BOOL isLightOn;
+@property (nonatomic, assign) float lightEnergy;
+@property (nonatomic, assign) GLKVector3 position;
+@property (nonatomic, assign) GLKVector3 ambientColor;
+@property (nonatomic, assign) GLKVector3 diffuseColor;
+@property (nonatomic, assign) GLKVector3 specularColor;
+@property (nonatomic, assign) GLKVector3 lightAttenuation;
+
+@end
---
+++ OBJ-C/GLPointLight.m
@@ -0,0 +1,79 @@
+//
+//  GLPointLight.m
+//  TestProject
+//
+//  Created by Рапоткин Николай on 05.02.18.
+//  Copyright (c) 2018 Rapotkin. All rights reserved.
+//
+
+#import "Math.h"
+#import "GLPointLight.h"
+
+@implementation BFGLPointLight
+
+-(id)init
+{
+	self = [super init];
+	if (self)
+	{
+		m_lightId = [StaticCounter count];
+        m_isLightOn = YES;
+		m_lightEnergy = 1.0;
+	}
+    
+	return self;
+}
+
+-(instancetype)pointLightWithPosition:(GLKVector3)position
+                              Ambient:(GLKVector3)ambient
+                              Diffuse:(GLKVector3)diffuse
+                             Specular:(GLKVector3)specular
+                          Attenuation:(GLKVector3)attenuation
+{
+    BFGLPointLight *light = [[[self class] alloc] init];
+    [light setPosition:position];
+    [light setAmbientColor:ambient];
+    [light setDiffuseColor:diffuse];
+    [light setSpecularColor:specular];
+    [light setLightAttenuation:attenuation];
+    
+    return light;
+}
+
+-(void)dealloc
+{
+}
+
+-(void)beforeDraw:(BFGLProgram *)program
+{
+	GLint isOn        = [program uniform:[NSString stringWithFormat:@"pointLights[%d].isOn", m_lightId]];
+	GLint position    = [program uniform:[NSString stringWithFormat:@"pointLights[%d].position", m_lightId]];
+	GLint ambient     = [program uniform:[NSString stringWithFormat:@"pointLights[%d].ambientColor", m_lightId]];
+	GLint diffuse     = [program uniform:[NSString stringWithFormat:@"pointLights[%d].diffuseColor", m_lightId]];
+	GLint specular    = [program uniform:[NSString stringWithFormat:@"pointLights[%d].specularColor", m_lightId]];
+	GLint attenuation = [program uniform:[NSString stringWithFormat:@"pointLights[%d].attenuation", m_lightId]];
+
+    glUniform1i(isOn, m_isLightOn ? 1 : 0);
+    glUniform3fv(position,    1, m_position.v);
+    glUniform3fv(ambient,     1, m_ambientColor.v);
+    glUniform3fv(diffuse,     1, m_diffuseColor.v);
+    glUniform3fv(specular,    1, m_specularColor.v);
+    glUniform3fv(attenuation, 1, m_lightAttenuation.v);
+}
+
+-(void)afterDraw:(BFGLProgram *)program
+{
+}
+
+-(void)draw:(BFGLProgram *)program
+{
+}
+
+@synthesize isLightOn = m_isLightOn,
+            lightEnergy = m_lightEnergy,
+            position = m_position,
+            ambientColor = m_ambientColor,
+            diffuseColor = m_diffuseColor,
+            specularColor = m_specularColor,
+            lightAttenuation = m_lightAttenuation;
+@end
